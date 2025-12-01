0bb704c83cf772c318c563d8574e9d1a30ebde35
Jtaz8681 <jtaz8681@gmail.com>
2025-11-30 23:57:10 -0600

    [dyad] Fixed Next.js build configuration issues - wrote 6 file(s)

---
+++ next-pwa.config.js
@@ -0,0 +1,25 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'https-calls',
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 150,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
+});
+
+module.exports = withPWA;
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-// @ts-ignore - next-pwa has type compatibility issues
-import withPWA from "next-pwa";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
@@ -21,26 +18,4 @@ const nextConfig: NextConfig = {
   turbopack: {},
 };
 
-export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'https-calls',
-        networkTimeoutSeconds: 15,
-        expiration: {
-          maxEntries: 150,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
-})(nextConfig);
+export default nextConfig;
